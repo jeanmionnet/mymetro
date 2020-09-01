e8152f6fbf4e999ebe3ed08ab7fea60698e0267a
jmionnet <jean.mionnet.simplon@gmail.com>
2020-09-01 15:52:24 +0200

    fav.js & fav.html wip

--- prod/fav.js
+++ prod/fav.js
@@ -1,25 +1,11 @@
 // CATEGORIES TRANSPORT
-const tram_section = $('#tram_section');
-let chrono_section = $("#chrono");
-let proximo_section = $("#proximo");
-let flexo_section = $("#flexo");
-let transisere_section = $("#transisere");
-let tougo_section = $("#tougo");
-let pays_voironnais_section = $("#pays_voironnais");
-let scolaire_section = $("#scolaire");
-let tad_section = $("#tad");
-let snc_section = $("#snc");
+const fav_box = $('#fav_box');
+const fav_list = localStorage.getItem('fav');
 
-// INIT MAP
-let mymap = L.map('lines_map').setView([45.188529, 5.724524], 13);
-
-// FIRST MAP LAYER
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+// ENLEVE LE TEXTE SI IL Y A DU CONTENU
 
 // RECUPERE TOUTES LES LIGNES DE LA REGION
-const lines = () => $.ajax({
+const fav = () => $.ajax({
     url: 'http://data.metromobilite.fr/api/routers/default/index/routes',
     type: "GET",
     dataType: "json",
@@ -35,15 +21,21 @@ const lines = () => $.ajax({
         const text_color= item.textColor;
         const id = item.id;
 
+        if (fav_list.includes(id)) {
+
         // ELEMENTS HTML
-        let circle = document.createElement('div');
+        let circle = document.createElement('a');
+        $(circle).attr({
+            'href': './line_details.html',
+            'alt': `${mode} ${name}`
+        });
         circle.id = id;
         let line = document.createElement('p');
 
         // TRAMS
         if (mode === "TRAM") {
             if (type === "TRAM") {
-                tram_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -55,7 +47,7 @@ const lines = () => $.ajax({
         {
             if (type === "CHRONO") {
             // CHRONO BUS
-                chrono_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -64,7 +56,7 @@ const lines = () => $.ajax({
                 line.innerText = `${name}`;
             } else if (type === "PROXIMO") {
             // PROXIMO BUS
-                proximo_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -73,7 +65,7 @@ const lines = () => $.ajax({
                 line.innerText = `${name}`;
             } else if (type === "FLEXO") {
             // FLEXO BUS
-                flexo_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -82,7 +74,7 @@ const lines = () => $.ajax({
                 line.innerText = `${name}`;
             } else if (type === "C38") {
             // BUS TRANSISERES
-                transisere_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -92,7 +84,7 @@ const lines = () => $.ajax({
             } else if (type === "Structurantes" 
             || type === "Secondaires") {
             // BUS TOUGO
-                tougo_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -102,7 +94,7 @@ const lines = () => $.ajax({
             } else if (type === "Urbaines" 
             || type === "Interurbaines") {
             // BUS PAYS VOIRONNAIS
-                pays_voironnais_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -111,7 +103,7 @@ const lines = () => $.ajax({
                 line.innerText = `${name}`;
             } else if (type === "TAD") {
             // BUS A LA DEMANDE
-                tad_section.append(circle);
+                fav_box.append(circle);
                 circle.append(line);
                 circle.className = 'circle';
                 circle.style.backgroundColor = `#${color}`;
@@ -127,68 +119,12 @@ const lines = () => $.ajax({
             localStorage.setItem('mode', mode);
             localStorage.setItem('color', color);
         })
+        }
     }; 
 }).fail((error) => {
     console.warn('FAILLLLL');
     console.log(error);
 })
 
-// Lance la recherche des lignes au démarrage & initialise la map
-$(document).ready(() => {
-    lines()
-});
-
-// LINE SELECTION
-$(document).on('click', '.circle', function () {
-    $(this).toggleClass('actived_line');
-
-    // RECUPERE LE TRACE DE LA LIGNE
-    $.ajax({
-        url: `http://data.metromobilite.fr/api/lines/json?types=ligne&codes=${this.id}`,
-        type: "GET",
-        dataType: "json",
-    }).done((data) => {
-        const color = `rgb(${data.features[0].properties.COULEUR})`;
-        const id = this.id;
-        console.log(data);
-
-        // CREE LA LIGNE GRACE AUX COORDONEES
-        let my_line = [{
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": data // LISTE DES COORDONNEES GPS
-            }
-        }];
-
-        // LUI DONNE UN STYLE
-        let myStyle = {
-            "color": color,
-            "weight": 5,
-            "opacity": 0.8
-        };
-
-        // L'AJOUTE A LA MAP
-        L.geoJSON(my_line, {
-            style: myStyle,
-            onEachFeature: (feature, layer) => {
-                layer.tag_id = id;
-            }
-        }).addTo(mymap);
-
-        // REMOVE LAYERS 
-        const remove = () => {
-            mymap.eachLayer((layer) => {
-                if ( layer.tag_id &&  layer.tag_id === id) {
-                    mymap.removeLayer(layer)
-                }
-            });
-        }
-
-        remove();
-
-    }).fail((error) => {
-        console.warn('FAILLLLL');
-        console.log(error);
-    })
-});
+// Lance la recherche des lignes au démarrage
+$(document).ready = fav();
